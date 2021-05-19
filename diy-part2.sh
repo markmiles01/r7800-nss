b8a500513ce7c2c52f775dae03d08809c4e2ce76
markmiles01 <80091285+markmiles01@users.noreply.github.com>
2021-05-19 01:22:04 -0500

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,30 +13,22 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.211.39.1/g' package/base-files/files/bin/config_generate
 
-# cat >> feeds/luci/modules/luci-base/po/zh_Hans/base.po <<EOF
-
-# #: modules/luci-base/root/usr/share/luci/menu.d/luci-base.json:45
-# msgid "NAS"
-# msgstr "网络存储"
-# EOF
-
-
-#cat >> .config <<EOF
-#CONFIG_KERNEL_BUILD_USER="mark"
-#CONFIG_KERNEL_BUILD_DOMAIN="stabledatasolutions.com"
-#EOF
+cat >> .config <<EOF
+CONFIG_KERNEL_BUILD_USER="mark"
+CONFIG_KERNEL_BUILD_DOMAIN="stabledatasolutions.com"
+EOF
 
 # R7800:
-#cat >> .config <<EOF
-#CONFIG_TARGET_ipq806x=y
-#CONFIG_TARGET_ipq806x_generic_DEVICE_netgear_r7800=y
-#EOF
+cat >> .config <<EOF
+CONFIG_TARGET_ipq806x=y
+CONFIG_TARGET_ipq806x_generic_DEVICE_netgear_r7800=y
+EOF
 
-# 设置固件大小:
-# cat >> .config <<EOF
-# CONFIG_TARGET_KERNEL_PARTSIZE=28
-# CONFIG_TARGET_ROOTFS_PARTSIZE=100
-# EOF
+cat >> .config <<EOF
+
+CONFIG_TARGET_KERNEL_PARTSIZE=28
+CONFIG_TARGET_ROOTFS_PARTSIZE=100
+EOF
 
 # 固件压缩:
 # cat >> .config <<EOF
@@ -147,7 +139,7 @@ CONFIG_PACKAGE_luci-app-accesscontrol=y
 CONFIG_PACKAGE_luci-app-adguardhome=y
 CONFIG_PACKAGE_luci-app-advanced=y
 CONFIG_PACKAGE_luci-app-airplay2=y
-CONFIG_PACKAGE_luci-app-airwhu=y
+CONFIG_PACKAGE_luci-app-airwhu=n
 CONFIG_PACKAGE_luci-app-aliddns=y
 CONFIG_PACKAGE_luci-app-amule=y
 CONFIG_PACKAGE_luci-app-argon-config=y
@@ -198,7 +190,7 @@ CONFIG_PACKAGE_luci-app-ipsec-vpnd=y
 CONFIG_PACKAGE_luci-app-ipsec-vpnserver-manyusers=y
 CONFIG_PACKAGE_luci-app-iptvhelper=y
 CONFIG_PACKAGE_luci-app-jd-dailybonus=y
-CONFIG_PACKAGE_luci-app-k3screenctrl=y
+CONFIG_PACKAGE_luci-app-k3screenctrl=n
 CONFIG_PACKAGE_luci-app-k3usb=y
 CONFIG_PACKAGE_luci-app-kcptun=y
 CONFIG_PACKAGE_luci-app-kodexplorer=y
@@ -237,8 +229,9 @@ CONFIG_PACKAGE_luci-app-pppoe-relay=y
 CONFIG_PACKAGE_luci-app-pppoe-server=y
 CONFIG_PACKAGE_luci-app-pptp-server=y
 CONFIG_PACKAGE_luci-app-ps3netsrv=y
-CONFIG_PACKAGE_luci-app-qbittorrent=y
-CONFIG_PACKAGE_luci-app-qosv4=y
+CONFIG_PACKAGE_luci-app-qbittorrent=n
+CONFIG_PACKAGE_qBittorrent-Enhanced-Edition=n
+CONFIG_PACKAGE_luci-app-qosv4=n
 CONFIG_PACKAGE_luci-app-ramfree=y
 CONFIG_PACKAGE_luci-app-rclone=y
 CONFIG_PACKAGE_luci-app-rosy-file-server=y
@@ -291,9 +284,28 @@ CONFIG_PACKAGE_luci-app-xunlei=y
 CONFIG_PACKAGE_luci-app-zerotier=y
 CONFIG_PACKAGE_luci-lib-docker=y
 CONFIG_PACKAGE_luci-lib-fs=y
+
+# Protocols
+CONFIG_PACKAGE_luci-proto-3g=y
 CONFIG_PACKAGE_luci-proto-bonding=y
+CONFIG_PACKAGE_luci-proto-gre=y
+CONFIG_PACKAGE_luci-proto-hnet=y
+CONFIG_PACKAGE_luci-proto-ipip=y
+CONFIG_PACKAGE_luci-proto-ipv6=y
 CONFIG_PACKAGE_luci-proto-minieap=y
+CONFIG_PACKAGE_luci-proto-modemmanager=y
+CONFIG_PACKAGE_luci-proto-ncm=y
 CONFIG_PACKAGE_luci-proto-netkeeper=y
+CONFIG_PACKAGE_luci-proto-openconnect=y
+CONFIG_PACKAGE_luci-proto-openfortivpn=y
+CONFIG_PACKAGE_luci-proto-ppp=y
+CONFIG_PACKAGE_luci-proto-pppossh=y
+CONFIG_PACKAGE_luci-proto-qmi=y
+CONFIG_PACKAGE_luci-proto-relay=y
+CONFIG_PACKAGE_luci-proto-sstp=y
+CONFIG_PACKAGE_luci-proto-vpnc=y
+CONFIG_PACKAGE_luci-proto-vxlan=y
+CONFIG_PACKAGE_luci-proto-wireguard=y
 EOF
 
 # luci app options
@@ -303,115 +315,61 @@ CONFIG_PACKAGE_luci-app-diskman_INCLUDE_lsblk=y
 CONFIG_PACKAGE_luci-app-dockerman_INCLUDE_ttyd=y
 EOF
 
-# DISABLE ANY ALREADY ENABLED OPTIONS
-sed -i 's/CONFIG_PACKAGE_luci-app-trojan-server=y/CONFIG_PACKAGE_luci-app-trojan-server=n/g' .config
-
-### TO ABLE TO USE STOCK OPKG'S ###
-### https://downloads.openwrt.org/releases/21.02.0-rc1/targets/ipq806x/generic/config.buildinfo ###
+# Libraries
 cat >> .config <<EOF
-CONFIG_TARGET_ipq806x=y
-CONFIG_TARGET_ipq806x_generic=y
-CONFIG_TARGET_MULTI_PROFILE=y
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_asrock_g10=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_asrock_g10=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_buffalo_wxr-2533dhp=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_buffalo_wxr-2533dhp=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_compex_wpq864=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_compex_wpq864=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_edgecore_ecw5410=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_edgecore_ecw5410=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_linksys_ea7500-v1=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_linksys_ea7500-v1=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_linksys_ea8500=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_linksys_ea8500=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_nec_wg2600hp=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_nec_wg2600hp=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_nec_wg2600hp3=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_nec_wg2600hp3=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_netgear_d7800=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_netgear_d7800=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_netgear_r7500=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_netgear_r7500=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_netgear_r7500v2=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_netgear_r7500v2=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_netgear_r7800=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_netgear_r7800=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_qcom_ipq8064-ap148-legacy=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_qcom_ipq8064-ap148-legacy=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_qcom_ipq8064-ap148=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_qcom_ipq8064-ap148=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_qcom_ipq8064-ap161=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_qcom_ipq8064-ap161=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_qcom_ipq8064-db149=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_qcom_ipq8064-db149=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_tplink_ad7200=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_tplink_ad7200=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_tplink_c2600=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_tplink_c2600=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_tplink_vr2600v=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_tplink_vr2600v=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_ubnt_unifi-ac-hd=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_ubnt_unifi-ac-hd=""
-CONFIG_TARGET_DEVICE_ipq806x_generic_DEVICE_zyxel_nbg6817=y
-CONFIG_TARGET_DEVICE_PACKAGES_ipq806x_generic_DEVICE_zyxel_nbg6817=""
-CONFIG_ALL_KMODS=y
-CONFIG_ALL_NONSHARED=y
-CONFIG_DEVEL=y
-CONFIG_TARGET_PER_DEVICE_ROOTFS=y
-CONFIG_AUTOREMOVE=y
-CONFIG_BUILDBOT=y
-CONFIG_COLLECT_KERNEL_DEBUG=y
-CONFIG_IB=y
-CONFIG_IMAGEOPT=y
-CONFIG_JSON_OVERVIEW_IMAGE_INFO=y
-CONFIG_KERNEL_BUILD_DOMAIN="buildhost"
-CONFIG_KERNEL_BUILD_USER="builder"
-# CONFIG_KERNEL_KALLSYMS is not set
-CONFIG_PACKAGE_cgi-io=y
-CONFIG_PACKAGE_libiwinfo-lua=y
-CONFIG_PACKAGE_liblua=y
-CONFIG_PACKAGE_liblucihttp=y
-CONFIG_PACKAGE_liblucihttp-lua=y
-CONFIG_PACKAGE_libubus-lua=y
-CONFIG_PACKAGE_lua=y
-CONFIG_PACKAGE_luci=y
-CONFIG_PACKAGE_luci-app-firewall=y
-CONFIG_PACKAGE_luci-app-opkg=y
-CONFIG_PACKAGE_luci-base=y
 CONFIG_PACKAGE_luci-lib-base=y
+CONFIG_PACKAGE_luci-lib-dracula=y
+CONFIG_PACKAGE_luci-lib-httpclient=y
+CONFIG_PACKAGE_luci-lib-httpprotoutils=y
 CONFIG_PACKAGE_luci-lib-ip=y
+CONFIG_PACKAGE_luci-lib-ipkg=y
+CONFIG_PACKAGE_luci-lib-iptparser=y
+CONFIG_PACKAGE_luci-lib-jquery-1-4=y
+CONFIG_PACKAGE_luci-lib-json=y
 CONFIG_PACKAGE_luci-lib-jsonc=y
 CONFIG_PACKAGE_luci-lib-nixio=y
-CONFIG_PACKAGE_luci-mod-admin-full=y
-CONFIG_PACKAGE_luci-mod-network=y
-CONFIG_PACKAGE_luci-mod-status=y
-CONFIG_PACKAGE_luci-mod-system=y
-CONFIG_PACKAGE_luci-proto-ipv6=y
-CONFIG_PACKAGE_luci-proto-ppp=y
-CONFIG_PACKAGE_luci-theme-bootstrap=y
-CONFIG_PACKAGE_rpcd=y
-CONFIG_PACKAGE_rpcd-mod-file=y
-CONFIG_PACKAGE_rpcd-mod-iwinfo=y
-CONFIG_PACKAGE_rpcd-mod-luci=y
-CONFIG_PACKAGE_rpcd-mod-rrdns=y
-CONFIG_PACKAGE_uhttpd=y
-CONFIG_PACKAGE_uhttpd-mod-ubus=y
-CONFIG_REPRODUCIBLE_DEBUG_INFO=y
-CONFIG_SDK=y
-CONFIG_TARGET_ALL_PROFILES=y
-CONFIG_VERSIONOPT=y
-CONFIG_VERSION_BUG_URL=""
-CONFIG_VERSION_CODE=""
-CONFIG_VERSION_DIST="OpenWrt"
-CONFIG_VERSION_FILENAMES=y
-CONFIG_VERSION_HOME_URL=""
-CONFIG_VERSION_HWREV=""
-CONFIG_VERSION_MANUFACTURER=""
-CONFIG_VERSION_MANUFACTURER_URL=""
-CONFIG_VERSION_NUMBER=""
-CONFIG_VERSION_PRODUCT=""
-CONFIG_VERSION_REPO="https://downloads.openwrt.org/releases/21.02.0-rc1"
-CONFIG_VERSION_SUPPORT_URL=""
+CONFIG_PACKAGE_luci-lib-nixio_notls=y
+CONFIG_PACKAGE_luci-lib-nixio_axtls=y
+CONFIG_PACKAGE_luci-lib-nixio_cyassl=y
+CONFIG_PACKAGE_luci-lib-nixio_openssl=y
+CONFIG_PACKAGE_luci-lib-docker=y
+CONFIG_PACKAGE_luci-lib-fs=y
+CONFIG_PACKAGE_luci-lib-px5g=y
 EOF
+
+
+# Luci statistics
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci-app-statistics=y
+CONFIG_PACKAGE_collectd-mod-conntrack=y
+CONFIG_PACKAGE_collectd-mod-cpufreq=y
+CONFIG_PACKAGE_collectd-mod-dhcpleases=y
+CONFIG_PACKAGE_collectd-mod-entropy=y
+CONFIG_PACKAGE_collectd-mod-exec=y
+CONFIG_PACKAGE_collectd-mod-interface=y
+CONFIG_PACKAGE_collectd-mod-iwinfo=y
+CONFIG_PACKAGE_collectd-mod-load=y
+CONFIG_PACKAGE_collectd-mod-memory=y
+CONFIG_PACKAGE_collectd-mod-network=y
+CONFIG_PACKAGE_collectd-mod-ping=y
+CONFIG_PACKAGE_collectd-mod-sqm=y
+CONFIG_PACKAGE_collectd-mod-thermal=y
+CONFIG_PACKAGE_collectd-mod-wireless=y
+CONFIG_PACKAGE_collectd-mod-uptime=y
+EOF
+
+# Zabbix
+#
+cat >> .config <<EOF
+CONFIG_ZABBIX_OPENSSL=y
+CONFIG_PACKAGE_zabbix-agentd=y
+CONFIG_PACKAGE_zabbix-get=y
+CONFIG_PACKAGE_zabbix-extra-mac80211=y
+CONFIG_PACKAGE_zabbix-extra-network=y
+CONFIG_PACKAGE_zabbix-extra-wifi=y
+EOF
+
+# DISABLE ANY ALREADY ENABLED OPTIONS
+sed -i 's/CONFIG_PACKAGE_luci-app-trojan-server=y/CONFIG_PACKAGE_luci-app-trojan-server=n/g' .config
 
 sed -i 's/^[ \t]*//g' ./.config
